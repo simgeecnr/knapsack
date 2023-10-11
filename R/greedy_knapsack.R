1749a94b145c4181288a71138dd65c59f1e95744
“simgeecnr” <csimge00@gmail.com>
2023-10-11 12:11:39 +0200

    Adding Greedy and DP

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,12 +1,52 @@
-#' Title
+#' Greedy Heuristic for Knapsack Problem
 #'
+#' @description The algorithm solves the knapsack problem using Greedy approach. Its computational 
+#' complexity is O(n*log(n))
+#' 
 #' @param x 
 #' @param W 
 #'
-#' @return
+#' @return It returns the maximum value obtained and selected items.
+#' @seealso [Knapsack Problem](https://en.wikipedia.org/wiki/Knapsack_problem#Greedy_approximation_algorithm)
 #' @export
 #'
 #' @examples
-greedy_knapsack <- function(x, W){
-  return(NULL)
+#' x <- data.frame(v = c(10, 5, 15, 7, 6, 18), w = c(2, 3, 5, 7, 1, 4))
+#' W <- 15
+#' greedy_knapsack(x,W)
+
+greedy_knapsack <- function(x, W) {
+  # Calculate the ratio
+  x$value_per_weight <- x$v / x$w
+  
+  # Sort items according to ratio
+  x <- x[order(-x$value_per_weight), ]
+  
+  # Initialize variables
+  n <- nrow(x)
+  knapsack <- rep(0, n)
+  total_value <- 0
+  remaining_capacity <- W
+  
+  # Fill the knapsack
+  for (i in 1:n) {
+    if (remaining_capacity == 0) {
+      break
+    }
+    
+    if (x$w[i] <= remaining_capacity) {
+      knapsack[i] <- 1
+      total_value <- total_value + x$v[i]
+      remaining_capacity <- remaining_capacity - x$w[i]
+    } else {
+      # Take a fraction of the item
+      fraction <- remaining_capacity / x$w[i]
+      knapsack[i] <- fraction
+      total_value <- total_value + fraction * x$v[i]
+      remaining_capacity <- 0
+    }
+  }
+  
+  selected <- which(knapsack == 1)
+  return(list(value = total_value, elements = selected))
 }
--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -1,12 +1,54 @@
-#' Title
+#' Dynammic Programming for Knapsack Problem
 #'
+#' @description The algorithm solves the knapsack problem using Dynammic Programming approach. 
+#' Its computational complexity is O(Wn).
 #' @param x 
 #' @param W 
 #'
-#' @return
+#' @return It returns the maximum value obtained and selected items.
+#' @seealso [Knapsack Problem](https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem)
 #' @export
 #'
 #' @examples
+#' x <- data.frame(v = c(10, 5, 15, 7, 6, 18), w = c(2, 3, 5, 7, 1, 4))
+#' W <- 15
+#' knapsack_dynamic(x,W)
+
 knapsack_dynamic <- function(x, W){
-  return(NULL)
+  #Initialize the matrix
+  n <- nrow(x)
+  m <- matrix(0, n+1, W+1)
+  
+  #Filling the matrix by calculating the values
+  for (i in 2:(n + 1)) { 
+    for (j in 2:(W + 1)) { 
+      if (x[i - 1, 'w'] > j) {
+        m[i, j] <- m[i - 1, j] 
+      } else {
+        m[i, j] <- max(m[i - 1, j], m[i - 1, j - x[i - 1, 'w']] + x[i - 1, 'v'])
+      }
+    }
+  }
+  
+  i <- n+1
+  w <- W+1
+  max_value <- m[i,w]
+  list_idx <- rep(0, n)
+  value_to_check <- m[i,w]
+  
+  #Checking which elements are selected
+  while (i>0){
+    upper_row <- m[i-1, ]
+    value_exists <- any(upper_row == value_to_check)
+    if (value_exists){
+      i <- i-1 
+    } else {
+      list_idx[i-1] <- 1
+      value_to_check <- value_to_check - x[i - 1, 'v']
+      i <- i-1
+    }
+  }
+  
+  selected <- which(list_idx == 1)
+  return(list(value = max_value, elements = selected))
 }
