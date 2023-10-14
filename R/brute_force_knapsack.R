d1554af079824a8c8f59f34cbfd3da54f55bf81f
Hugo Morvan <98044151+hugo-morvan@users.noreply.github.com>
2023-10-14 14:30:28 +0200

    fixed brute force and combined dynamics functions into 1.

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -12,7 +12,7 @@
 #' W <- 15
 #' brute_force_knapsack(x,W)
 
-brute_force_knapsack <- function(x, W, parallel=FALSE){
+brute_force_knapsack <- function(x, W){
   
   #=== Input Check =========
   #The function should check that the inputs are correct (i.e. a data.frame with two variables v and w)
@@ -21,14 +21,12 @@ brute_force_knapsack <- function(x, W, parallel=FALSE){
   stopifnot(is.data.frame(x))
   stopifnot("v" %in% colnames(x) && "w" %in% colnames(x))
 
-
 #=== Helper functions================================
   get_element_idx <- function(bin_rep){
     #given a binary representation, return the indexes of the 1s in the bin_rep
     bin_rep_split <- strsplit(bin_rep, "")[[1]] # https://stackoverflow.com/questions/26721340/iterating-over-characters-of-string-r
     indexes <- c()
     j <- 1
-    #print(bin_rep)
     for (i in 1:32){
       if (bin_rep_split[i] == "1"){
         indexes[j] <- i
@@ -41,7 +39,6 @@ brute_force_knapsack <- function(x, W, parallel=FALSE){
   get_comb_info <- function(bin_rep, x){
     #given a bin_rep and a dataframe with v and w , returns the value and weight of the specified combination of items
     indexes <- get_element_idx(bin_rep)
-    print(indexes)
     tot_value <- 0
     tot_weight <- 0
     for (i in 1:length(indexes)){
@@ -50,7 +47,6 @@ brute_force_knapsack <- function(x, W, parallel=FALSE){
       tot_weight <- tot_weight + weight
       tot_value <- tot_value + value
     }
-    cat("weight:", tot_weight, "value:", tot_value, "\n")
     return(c(weight = tot_weight, value = tot_value, bin_rep = bin_rep))
   }
   
@@ -66,171 +62,47 @@ brute_force_knapsack <- function(x, W, parallel=FALSE){
   recursive_knap <- function(i, left_idx, right_idx, x, W){
     #i is my vector from 1:2^N
     if(left_idx == right_idx){
-      cat("i =", i[left_idx], "\n")
       #Convert i to binary
       binary <- as.integer(intToBits(i[left_idx]))
       bin_rep <- paste(binary, collapse = "")
       #Fetch the infos
       info <- get_comb_info(bin_rep, x)
-      #print(info)
       if(as.numeric(info[1]) <= W){ #Check if solution is under weight limit
         return(info)
       }
       else{
-        #cat("Too Heavy", info[1], "\n")
         return(c(weight = 0,value = 0,bin_rep = 0))
       }
     }
     else{
+      #Split the set in 2 
       middle <- (left_idx+right_idx)%/%2
-      
       left_info <- recursive_knap(i, left_idx, middle, x, W)
       right_info <- recursive_knap(i, middle+1, right_idx, x, W)
       
       #Calculate the max of left and right
       left_max_value <- as.numeric(left_info[2])
-      
       right_max_value <- as.numeric(right_info[2])
-      #cat("Left:", left_max_value, "Right;", right_max_value, "\n")
       #return the max value
       if(left_max_value > right_max_value){
-        #cat("returning left \n \n")
         return(left_info)
       }else{
-        #cat("returning right \n \n ")
         return(right_info)
       }
     }
   }
-  
-  if(parallel){
-    #=== Parallel Section ===============================
-    print("Parallel selected")
-    #Source Detect OS: https://stackoverflow.com/questions/4463087/detecting-operating-system-in-r-e-g-for-adaptive-rprofile-files
-    switch(Sys.info()[['sysname']],
-           Windows= {
-             print("--- Starting to run Parallel for Windows code: ---")
-             #Parallel workflow source : https://www.blasbenito.com/post/02_parallelizing_loops_with_r/
-             
-             library(parallel)
-             library(doParallel)
-             
-             nCores <- parallel::detectCores() #Leave out one core for safety
-             cat("Number of cores detected:", nCores, "\n")
-             
-             #Create the cluster, necessary for the %dopar%, keep 1 core for safety
-             cl <- parallel::makeCluster(nCores)
-             
-             #register the cluster
-             doParallel::registerDoParallel(cl = cl)
-             
-             #print(foreach::getDoParRegistered())
-             n <- (2^(length(x$v))-1)
-             cat("Number of combinations:", n, "\n")
-             # -- Parallel For each --
-             result <- foreach(
-               i = 1:(2^(length(x$v))-1)
-             ) %dopar% {
-               print("spawn")
-               recursive_knap(i = i, left_idx = 1, right_idx = n, x = x, W = W)
-             }
-             parallel::stopCluster(cl = cl)
-             print(result)
-             #max_value <- as.numeric(result[2])
-             #max_value_elements <- get_element_idx(result[1])
-             answer <- c(weight = 0,value = 0,bin_rep = 0)
-             #answer <- list(value = round(max_value, 0), elements = max_value_elements)
-             },
-           Linux  = {print("Parallel is function is not implemented for Linux yet")
-             answer <- 0
-             },
-           Darwin = {print("Parallel is function is not implemented for Mac yet")
-             #Parallel workflow source : https://www.blasbenito.com/post/02_parallelizing_loops_with_r/
-             
-             library(parallel)
-             library(doParallel)
-             
-             nCores <- parallel::detectCores() #Leave out one core for safety
-             cat("Number of cores detected:", nCores, "\n")
-             
-             #Create the cluster, necessary for the %dopar%, keep 1 core for safety
-             cl <- parallel::makeCluster(nCores)
-             
-             #register the cluster
-             doParallel::registerDoParallel(cl = cl)
-             
-             #print(foreach::getDoParRegistered())
-             
-             # -- Parallel For each --
-             para_work <- foreach(
-               i = 1:(2^(length(x$v))-1),
-               .combine = 'data.frame' #options : c, cbind, rbind, list , data.frame?
-             ) %dopar% {
-               # Calculate the binary representation
-               binary <- intToBits(i)
-               binary <- as.integer(binary)
-               bin_rep <- paste(binary, collapse = "")
-               
-               #Get de combination information (weight, value, bin_rep)
-               g <- get_comb_info(bin_rep, x)
-             }
-             parallel::stopCluster(cl = cl)
-             #print(para_work)
-             #max_value_id <- find_max_value_row(para_work_df, W)
-             print(para_work)
-             
-             #Filter out the results where the weight is above the maximum allowed weight
-             filtered_df <- para_work[para_work$weight <= W, ]
-             print(filtered_df)
-             
-             #Find the row with the maximum value
-             maxValueRowIndex <- which.max(filtered_df$value)
-             maxValueID <- filtered_df$bin_rep[maxValueRowIndex]
-             print(maxValueID)
-             
-             #Get the information about the best combination
-             answer_obj <- get_comb_info(bin_rep = maxValueID, x)
-             max_value <- as.numeric(answer_obj[2])
-             max_value_elements <- get_element_idx(answer_obj[3])
-             answer <- list(value = round(as.numeric(max_value), 0), elements = max_value_elements)
-             })
-  }
 
-  else{
-    #=== Brute Force Section ============================
+#=== Brute Force Section ============================
     
-    #--- Initialize all the possible combinations ------
-    combinations <- c()
-    for (i in 1:2^(length(x$v))){ #Generate all 2^N -1 combinations
-      binary <- intToBits(i)
-      binary <- as.integer(binary)
-      bin_rep <- paste(binary, collapse = "")
-      combinations <- append(combinations, bin_rep)
-    }
-    combinations <- head(combinations, -1) #Get rid of the last combination (overflow !)
-    #--- Set the best option to taking No item ----------
-    max_value <- 0
-    max_value_elements <- intToBits(0)
+  #--- Set the best option to taking No item ----------
+  max_value <- 0
+  max_value_elements <- intToBits(0)
     
-    #--- Calculate every possible combination --------
-    # for (comb in combinations){
-    #   #Get the weight of the combinations of items
-    #   new_res <- get_comb_info(comb, x)
-    #   #Check if it is a better option
-    #   if(as.numeric(new_res[2]) > max_value && as.numeric(new_res[1]) <= W){
-    #     max_value <- as.numeric(new_res[2])
-    #     max_value_elements <- get_element_idx(comb)
-    #   }
-    # }
-    # 
-    n <- (2^(length(x$v))-1)
-    info <- recursive_knap(c(1:n),left_idx=1, right_idx=n, x=x, W=W)
-    max_value <- as.numeric(info[2])
-    max_value_elements <- get_element_idx(info[3])
-    #I dont know why but values have decimals, so I round the number to get rid of the issue
-    answer <- list(weight = as.numeric(info[1]), value = round(max_value, 0), elements = max_value_elements)
-  }
-  
-#=== Return the answer found in either Parallel or not =========
+  n <- (2^(length(x$v))-1) #The number of combinations to try
+  info <- recursive_knap(c(1:n),left_idx=1, right_idx=n, x=x, W=W)
+  max_value <- as.numeric(info[2])
+  max_value_elements <- get_element_idx(info[3])
+  answer <- list(value = round(max_value, 0), elements = max_value_elements)
+
   return(answer)
 }
--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -19,54 +19,91 @@ knapsack_dynamic <- function(x, W, fast = FALSE){
   source("R/rcpp_dynamic.R")
   if (fast){
     x <- as.matrix(x)
-    knapsack_dynamic_cpp(x, W)
-  }
-  #Initialize the matrix
-  n <- nrow(x)
-  m <- matrix(0, n+1, W+1)
+    results <- Rcpp::cppFunction('
+List knapsack_dynamic_cpp(NumericMatrix x, int W) {
+  int n = x.nrow();
+  NumericMatrix m(n + 1, W + 1);
   
-  #Filling the matrix by calculating the values
-  for (i in 2:(n + 1)) { 
-    for (j in 2:(W + 1)) { 
-      if (x[i - 1, 'w'] > j) {
-        m[i, j] <- m[i - 1, j] 
+  for (int i = 1; i <= (n); i++) {
+    for (int j = 1; j <= (W); j++) {
+      if (x(i - 1,0) > j) {
+        m(i, j) = m(i - 1, j);
       } else {
-        m[i, j] <- max(m[i - 1, j], m[i - 1, j - x[i - 1, 'w']] + x[i - 1, 'v'])
+        m(i, j) = std::max(m(i - 1, j), m(i - 1, j - x(i - 1, 0)) + x(i - 1, 1));
       }
     }
   }
-  i <- n+1
-  w <- W+1
-  max_value <- m[i,w]
-  list_idx <- rep(0, n)
-  value_to_check <- m[i,w]
+
+  int i = n;
+  int w = W;
+  double max_value = m(i, w);
   
-  #Checking which elements are selected
-  while (i>0){
-    upper_row <- m[i-1, ]
-    value_exists <- any(upper_row == value_to_check)
-    if (value_exists){
-      i <- i-1 
+  IntegerVector list_idx(n);
+  double value_to_check = m(i, w);
+  
+  while (i > 0) {
+    NumericVector upper_row = m(i - 1, _);
+    bool value_exists = std::any_of(upper_row.begin(), upper_row.end(), [&](double val) { return val == value_to_check; });
+    if (value_exists) {
+      i--;
     } else {
-      list_idx[i-1] <- 1
-      value_to_check <- value_to_check - x[i - 1, 'v']
-      i <- i-1
+      list_idx(i - 1) = 1;
+      value_to_check -= x(i - 1, 1);
+      i--;
     }
   }
-  
-  selected <- which(list_idx == 1)
-  t <- list(value = max_value, elements = selected)
-  return(t)
-}
 
-RNGversion(min(as.character(getRversion()),"3.5.3"))
-set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
-n <- 2000
-knapsack_objects <-
-  data.frame(
-    w=sample(1:4000, size = n, replace = TRUE),
-    v=runif(n = n, 0, 10000)
-  )
+  IntegerVector selected;
+  for (int i = 0; i < n; i++) {
+    if (list_idx[i] == 1) {
+      selected.push_back(i + 1);
+    }
+  }
+  List result = List::create(_["value"] = max_value, _["elements"] = selected);
+  return result;
+}
+')
+    return(results)
+  }
+  else{
+    #Initialize the matrix
+    n <- nrow(x)
+    m <- matrix(0, n+1, W+1)
+    
+    #Filling the matrix by calculating the values
+    for (i in 2:(n+1)) { 
+      for (j in 2:(W+1)) { 
+        if (x[i-1, 'w'] > j) {
+          m[i, j] <- m[i-1, j] 
+        } else {
+          m[i, j] <- max(m[i-1, j], m[i-1, j - x[i-1, 'w']] + x[i-1, 'v'])
+        }
+      }
+    }
+    i <- n+1
+    w <- W+1
+    max_value <- m[i,w]
+    list_idx <- rep(0, n)
+    value_to_check <- m[i,w]
+    
+    #Checking which elements are selected
+    while (i>0){
+      upper_row <- m[i-1, ]
+      value_exists <- any(upper_row == value_to_check)
+      if (value_exists){
+        i <- i-1 
+      } else {
+        list_idx[i-1] <- 1
+        value_to_check <- value_to_check - x[i - 1, 'v']
+        i <- i-1
+      }
+    }
+    
+    selected <- which(list_idx == 1)
+    t <- list(value = max_value, elements = selected)
+    return(t)
+  }
+}
 
 knapsack_dynamic(x = knapsack_objects[1:20,], W = 3500)
 knapsack_dynamic(x = knapsack_objects[1:20,], W = 3500, fast = TRUE)
